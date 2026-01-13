145b8f5984a989ce323678ee9a37340e6ed88d5f
Hortense <hchevalier.capnum@gmail.com>
2026-01-13 23:10:40 +0100

    Ajout membre : mireillecaulier.js

---
+++ membres/mireillecaulier.js
@@ -0,0 +1,22 @@
+export const mireillecaulier = {
+    id: "mireillecaulier",
+    prenom: "Mireille",
+    nom: "Caulier",
+    nom_code: "Aigle de feu",
+    poste: "Retraitée",
+    role_supp: "Référente PP85",
+    ville: "Mouilleron-le-Captif",
+    codePostal: "85000",
+    photo: "https://media.licdn.com/dms/image/v2/C5603AQGNzVood62LWQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1517802702208?e=2147483647&v=beta&t=Ynun2eSHqDve3g2sNN7USyuyItQzXwjd1zEQN0yguNs",
+    bio: "Ma vie mon oeuvre",
+    competences: ["Gestion RH"],
+    sujets_interet: ["Eolien"],
+    stats: {
+        "bagou": 3,
+        "redac": 3,
+        "terrain": 3,
+        "orga": 3
+    },
+    linkedin: "https://fr.linkedin.com/in/mireille-caulier-b55634110",
+    email: "caulierm@sfr.fr"
+};
