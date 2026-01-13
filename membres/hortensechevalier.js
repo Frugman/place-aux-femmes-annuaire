a5ae9e63986a819dfefdf6530a10afb7c59dfb00
Hortense <hchevalier.capnum@gmail.com>
2026-01-13 23:05:38 +0100

    Ajout membre : hortensechevalier.js

--- membres/hortensechevalier.js
+++ membres/hortensechevalier.js
@@ -1,30 +1,22 @@
 export const hortensechevalier = {
-    id: "hortensechevalier", // Identifiant unique (slug)
+    id: "hortensechevalier",
     prenom: "Hortense",
     nom: "Chevalier",
     nom_code: "Buse du marais",
     poste: "Consultante data et innovation",
     role_supp: "Co-référente PP85",
-    
     ville: "La Roche-sur-Yon",
     codePostal: "85000",
-    lat: 46.6705,
-    lng: -1.4260,
-    
     photo: "https://capnum.io/wp-content/uploads/2024/01/IMG_20210416_120814-1024x1024.jpg",
-    
-    bio: "J'accompagne quelques entreprises et des personnes publiques en data et innovation, j'ai aussi un blog sur la data et l'environnement qui me passionne.",
-    
-    competences: ["Stratégie Data", "Open Data", "Facilitation", "Gestion de projet", "Environnement", "IA vibe-coding", "Robotique", "Webmarketing"],
-    
-    // Vos scores (sur 5)
+    bio: "J'accompagne quelques entreprises et des personnes publiques en data et innovation, j'ai aussi un blog sur la data et l'environnement qui me passionne vraiment.",
+    competences: ["Stratégie Data","Open Data","Facilitation","Gestion de projet","Environnement","IA vibe-coding","Robotique","Webmarketing"],
+    sujets_interet: [],
     stats: {
-        bagou: 5,
-        redac: 4, 
-        terrain: 3,
-        orga: 5 
+        "bagou": 4,
+        "redac": 4,
+        "terrain": 3,
+        "orga": 5
     },
-
     linkedin: "https://www.linkedin.com/in/hortensechevalier/",
     email: "consultant.numerique@gmail.com"
 };
