c0ef9abcc933c822e083005fa19de342b7f1d197
Hortense <hchevalier.capnum@gmail.com>
2026-01-13 21:15:47 +0100

    Update index.js

--- membres/index.js
+++ membres/index.js
@@ -1,6 +1,6 @@
 // 1. Importer les fiches des membres ici
 // Attention : le nom entre accolades {} doit être EXACTEMENT le même que dans le fichier du membre
-import { hortenseChevalier } from './hortensechevalier.js';
+import { hortensechevalier } from './hortensechevalier.js';
 
 // 2. Ajouter les membres dans la liste finale
 export const lotsOfMembers = [
