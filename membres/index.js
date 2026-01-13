cc5f6decb2fba1a3616d90645969389feb1579a6
Hortense <hchevalier.capnum@gmail.com>
2026-01-14 00:07:38 +0100

    Auto-update index pour agnescamarena

--- membres/index.js
+++ membres/index.js
@@ -1,3 +1,4 @@
+import { agnescamarena } from './agnescamarena.js';
 import { delphinepaulot } from './delphinepaulot.js';
 import { mireillecaulier } from './mireillecaulier.js';
 // 1. Importer les fiches des membres ici
@@ -6,6 +7,7 @@ import { hortensechevalier } from './hortensechevalier.js';
 
 // 2. Ajouter les membres dans la liste finale
 export const lotsOfMembers = [
+    agnescamarena,
     delphinepaulot,
         mireillecaulier,
     hortensechevalier,
