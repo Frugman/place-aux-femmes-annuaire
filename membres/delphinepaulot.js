0c63f47a7383018791ada563cd8acfe645f800e2
Hortense <hchevalier.capnum@gmail.com>
2026-01-13 23:43:31 +0100

    Ajout membre : delphinepaulot.js

--- membres/delphinepaulot.js
+++ membres/delphinepaulot.js
@@ -9,7 +9,7 @@ export const delphinepaulot = {
     codePostal: "",
     lat: 47.311114,
     lng: -1.63115,
-    photo: "https://www.notion.so/image/https%3A%2F%2Fstorage.tally.so%2Fprivate%2FCapture-d-ecran-2025-12-22-a-10.11.15.png%3Fid%3DNMAJWG%26accessToken%3DeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ik5NQUpXRyIsImZvcm1JZCI6Ik5wRDREcCIsImlhdCI6MTc2NjM5NDcwMX0.solgx7bIre_ElrqrfWijHHWOhnv4F70gWPRdwgzqYWk%26signature%3Ddb686ae22a4541c48bd7a89f8a60439241a5e1aa9a25b43c5fda55e9faef2a7d?table=block&id=2d1a11f7-eccf-811c-a454-da2adb209afc&spaceId=497a11f7-eccf-81de-9190-0003ae904935&width=1750&userId=24fd872b-594c-81f6-a03a-0002721ef6cb&cache=v2",
+    photo: "https://media.licdn.com/dms/image/v2/C4D03AQFhQhWqn8pc3Q/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1594360355362?e=2147483647&v=beta&t=3oVrhVcWxyLqA6tBnN_sOJjLbdfOEm0thR67JvkzkSI",
     bio: "Ma vie mon oeuvre",
     competences: ["Curiosité"],
     sujets_interet: ["Justice sociale","Interculturalité"],
