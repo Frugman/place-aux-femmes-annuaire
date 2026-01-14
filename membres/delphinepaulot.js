57d402ae1e8203330e623559498a36bd54895044
Hortense <hchevalier.capnum@gmail.com>
2026-01-14 09:45:16 +0100

    Ajout membre : delphinepaulot.js

--- membres/delphinepaulot.js
+++ membres/delphinepaulot.js
@@ -2,23 +2,22 @@ export const delphinepaulot = {
     id: "delphinepaulot",
     prenom: "Delphine",
     nom: "Paulot",
-    nom_code: "Lionne du désert",
+    nom_code: "OISEAU du désert",
     poste: "Cheffe de projet",
-    role_supp: "",
+    role_supp: "Membre de la PPR",
     ville: "Treillières",
-    codePostal: "",
     lat: 47.311114,
     lng: -1.63115,
-    photo: "https://media.licdn.com/dms/image/v2/C4D03AQFhQhWqn8pc3Q/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1594360355362?e=2147483647&v=beta&t=3oVrhVcWxyLqA6tBnN_sOJjLbdfOEm0thR67JvkzkSI",
-    bio: "Ma vie mon oeuvre",
-    competences: ["Curiosité"],
-    sujets_interet: ["Justice sociale","Interculturalité"],
+    photo: "https://drive.google.com/drive/folders/1B_5cQ60pT7XVtL48zAfB6mGqRPUcJp5U",
+    bio: "Depuis 2013 pour MSF comme coordinatrice des RH sur les terrains puis Cheffe de projet pour le siège en Belgique et en France. Auparavant, manager dans les RH et le conseil, dans le privé pendant 15 ans. J'ai appris à comprendre l'importance de nos différences et qu'on s'enrichit en partageant.",
+    competences: ["Curiosité 🤓","peur de rien 🤠"],
+    sujets_interet: ["Justice sociale","écologie (agriculture","eau...)","antifa :)"],
     stats: {
         "bagou": 3,
         "redac": 4,
         "terrain": 4,
         "orga": 4
     },
-    linkedin: "",
-    email: ""
+    linkedin: "https://www.linkedin.com/in/dpaulot/",
+    email: "delphine.paulot@hotmail.fr"
 };
