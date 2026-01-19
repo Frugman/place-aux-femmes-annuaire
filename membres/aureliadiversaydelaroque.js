2d0e16505ea5a4688ed1c330d3e98b212efddcb6
Hortense <hchevalier.capnum@gmail.com>
2026-01-19 21:58:12 +0100

    Ajout membre : aureliadiversaydelaroque.js

---
+++ membres/aureliadiversaydelaroque.js
@@ -0,0 +1,27 @@
+export const aureliadiversaydelaroque = {
+    id: "aureliadiversaydelaroque",
+    prenom: "Aurélia",
+    nom: "DIVERSAY-DELAROQUE",
+    nom_code: "",
+    poste: "Avocate",
+    role_supp: "Déléguée mobilisation PPR PDL",
+    ville: "Saint Etienne de Montluc 44",
+    lat: 47.272362,
+    lng: -1.783443,
+    photo: "https://ui-avatars.com/api/?name=Aurélia+DIVERSAY-DELAROQUE&background=random&color=fff&size=256",
+    bio: "46 ans
+Avocate depuis 2005 au Barreau de Nantes en urbanisme fonction publique environnement côté agents associations particuliers
+Mère de 3 filles 17-13-9 ans
+Investie sur ma commune au sein de L’amicale laique section art de la rue (jonquilles en fêtes) et section culturelle (festival bouge montluc)
+Adhérente PP depuis oct 2023, réferente départementale 02/2024 septembre 2025. Adhérente au GNSA.",
+    competences: ["Prise de parole - organisation de rencontres et échanges","contact facile pour tracter"],
+    sujets_interet: ["Justice","Environnement","la connerie"],
+    stats: {
+        "bagou": 5,
+        "redac": 3,
+        "terrain": 4,
+        "orga": 2
+    },
+    linkedin: "",
+    email: "adiversay@gmail.com"
+};
