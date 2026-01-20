6cae8eefdc1a60153c088f8083c57ca86cfa6bf2
Hortense <hchevalier.capnum@gmail.com>
2026-01-20 17:17:56 +0100

    Ajout membre : soniaseibelbechaz.js

---
+++ membres/soniaseibelbechaz.js
@@ -0,0 +1,24 @@
+export const soniaseibelbechaz = {
+    id: "soniaseibelbechaz",
+    prenom: "Sonia",
+    nom: "Seibel-Béchaz",
+    nom_code: "Flower power",
+    poste: "Enseignante",
+    role_supp: "",
+    ville: "Rezé",
+    lat: 47.17269,
+    lng: -1.552789,
+    photo: "https://ui-avatars.com/api/?name=Sonia+Seibel-Béchaz&background=random&color=fff&size=256",
+    // On remplace les guillemets ET les sauts de ligne (qui cassent le code)
+    bio: "Née à Berlin, j'ai grandi dans le sud de l'Allemagne, à Lyon et un peu en Californie. De parents militants, je voulais après le bac changer le monde et ai fait des études de sciences politiques. Après une dizaine d'années de vie professionnelle dans ce domaine, entre autre en tant qu'assistante parlementaire à Berlin, j'ai choisi de devenir enseignante. Un métier où je prends plaisir et où je me sens utile. Européenne jusqu'au bout des ongles, mes collégues me surnomment \"militante pour l'Europe\". Je suis dans l'équipe Erasmus de mon lycée et tiens à faire bénéficier un maximum de nos 2000 élèves et collègues d'une telle expérience internationale qui représente pour moi une réelle éducation à la citoyenneté et à la paix. Maman de 2 garçons de 16 et 23 ans qui ont grandis des deux côtés du Rhin, j'ai hésite à me donner le nom de code \"louve du Rhin\".",
+    competences: ["Évènementiel","Organisation","Représentation","Communication","Relationnel","Lobbying","Anglais","Allemand"],
+    sujets_interet: ["Démocratie","Féminisme","Europe","Ecologie","Anti-Fascisme","Justice sociale","Paix..."],
+    stats: {
+        "bagou": 3,
+        "redac": 3,
+        "terrain": 4,
+        "orga": 5
+    },
+    linkedin: "",
+    email: "sonia.seibel@gmx.de"
+};
