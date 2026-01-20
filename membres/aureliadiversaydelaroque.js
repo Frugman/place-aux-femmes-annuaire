cdb6a6b120df379066c364ab5283a6380578a2f3
Hortense <hchevalier.capnum@gmail.com>
2026-01-20 22:01:51 +0100

    Ajout membre : aureliadiversaydelaroque.js

--- membres/aureliadiversaydelaroque.js
+++ membres/aureliadiversaydelaroque.js
@@ -10,7 +10,7 @@ export const aureliadiversaydelaroque = {
     lng: -1.783443,
     photo: "https://www.antigone-avocats.com/wp-content/uploads/2021/05/0117_Helene_Charier_Photographe_-_Antigone_-_Web-22.jpg",
     // On remplace les guillemets ET les sauts de ligne (qui cassent le code)
-    bio: "46 ans - Avocate depuis 2005 au Barreau de Nantes en urbanisme fonction publique environnement côté agents associations particuliers - Mère de 3 filles 17-13-9 ans - Investie sur ma commune au sein de L’amicale laique section art de la rue (jonquilles en fêtes) et section culturelle (festival bouge montluc) - Adhérente PP depuis oct 2023, réferente départementale 02/2024 septembre 2025. Adhérente au GNSA.",
+    bio: "46 ans - Avocate depuis 2005 au Barreau de Nantes en urbanisme fonction publique environnement côté agents associations particuliers - Mère de 3 filles 17-13-9 ans - Investie sur ma commune au sein de L’amicale laique section art de la rue (jonquilles en fêtes) et section culturelle (festival bouge montluc) - Adhérente PP depuis oct 2023, réferente départementale 02/2024 septembre 2025.",
     competences: ["Prise de parole - organisation de rencontres et échanges","contact facile pour tracter"],
     sujets_interet: ["Justice","Environnement","la connerie"],
     stats: {
