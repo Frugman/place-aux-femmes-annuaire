82d3f2e861c08c921949d06ba6b56d8be88cd1cf
Hortense <hchevalier.capnum@gmail.com>
2026-01-20 19:59:08 +0100

    Ajout membre : francoiseberric.js

--- membres/francoiseberric.js
+++ membres/francoiseberric.js
@@ -9,14 +9,15 @@ export const francoiseberric = {
     lat: 47.275629,
     lng: -1.437708,
     photo: "https://ui-avatars.com/api/?name=Françoise+Berric&background=random&color=fff&size=256",
+    // On remplace les guillemets ET les sauts de ligne (qui cassent le code)
     bio: "Après une formation scientifique,  je suis devenue enseignante spécialisée dans les difficultés d'apprentissage. Passionnée de théâtre, je joue depuis 40 ans dans une troupe  et porte aujourd'hui des projets théâtre  avec les institutions . Les valeurs qui me sont chères sont celles d'équité, d'inclusion, de respect, que je défends entre autres par le théâtre et la médiation culturelle.",
     competences: ["Mise en voix","interprétation","animation théâtrale"],
     sujets_interet: ["Égalité des chances","inclusion","justice sociale"],
     stats: {
-        "bagou": 3,
-        "redac": 4,
+        "bagou": 2,
+        "redac": 3,
         "terrain": 3,
-        "orga": 3
+        "orga": 2
     },
     linkedin: "https://www.linkedin.com/in/fran%C3%A7oise-berric-78029b265",
     email: "francoiseberric@gmail.com"
