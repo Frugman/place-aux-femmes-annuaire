f2efe75c6e86046920d171cd43f7d0d3dbb982ca
Hortense <hchevalier.capnum@gmail.com>
2026-02-01 21:10:38 +0100

    Ajout membre : annelaurencepayen.js

---
+++ membres/annelaurencepayen.js
@@ -0,0 +1,24 @@
+export const annelaurencepayen = {
+    id: "annelaurencepayen",
+    prenom: "Anne-Laurence",
+    nom: "PAYEN",
+    nom_code: "Utopiste engagee",
+    poste: "Médecin anesthésiste",
+    role_supp: "Adhérente",
+    ville: "Saint Nazaire",
+    lat: 47.275247,
+    lng: -2.242494,
+    photo: "https://ui-avatars.com/api/?name=Anne-Laurence+PAYEN&background=random&color=fff&size=256",
+    // On remplace les guillemets ET les sauts de ligne (qui cassent le code)
+    bio: "52 ans, médecin salariée, mère de 2 jeunes adultes. Investie dans plusieurs assos de ma ville. Adhérente PP, en manque d’enthousiasme actuellement.  Pas toujours au clair avec le féminisme d’opposition.  Déteste les conflits",
+    competences: ["Points de suture"],
+    sujets_interet: ["Écologie","lutte contre la précarité","égalité"],
+    stats: {
+        "bagou": 2,
+        "redac": 4,
+        "terrain": 3,
+        "orga": 4
+    },
+    linkedin: "",
+    email: "annelauric@yahoo.fr"
+};
